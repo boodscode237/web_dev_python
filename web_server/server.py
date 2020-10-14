0265218e5a47400909581ad98e082f7036289c6c
boodscode237 <boodscode@gmail.com>
2020-10-14 14:21:31 +0500

    learnend how to add icons in python

--- web_server/server.py
+++ web_server/server.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/blog')
+def blog():
+    return 'These are my thoughts on blogs'
+
+
+@app.route('/blog/2020/dogs')
+def blog2():
+    return 'These is my dog'
